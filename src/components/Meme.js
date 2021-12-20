95e93176682367bdd1c1aaa1eb1f21c26d293c01
abulyousuf <abulyousuf@btinternet.com>
2021-12-20 13:38:53 +0000

    Update meme component

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,14 +1,23 @@
 import React from "react";
 import "./Meme.css";
+import { memesData } from "../memesData";
 
 const Meme = () => {
+  const getMemeImage = () => {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    console.log(randomNumber);
+  };
+
   return (
     <main className="main">
-      <form className="form">
+      <div className="form">
         <input className="form--input" type="text" placeholder="Top text" />
         <input className="form--input" type="text" placeholder="Bottom text" />
-        <button className="form--btn">Get a new meme image 🖼</button>
-      </form>
+        <button onClick={getMemeImage} className="form--btn">
+          Get a new meme image 🖼
+        </button>
+      </div>
     </main>
   );
 };
