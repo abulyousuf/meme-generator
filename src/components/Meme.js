92a4678a8920921959a7d68207d5248dafadb91a
abulyousuf <abulyousuf@btinternet.com>
2021-12-24 03:06:49 +0000

    Use async function inside useEffect

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -10,10 +10,21 @@ const Meme = () => {
 
   const [allMemes, setAllMemes] = useState([]);
 
+  // useEffect(() => {
+  //   fetch("https://api.imgflip.com/get_memes")
+  //     .then(res => res.json())
+  //     .then(data => setAllMemes(data.data.memes));
+  // }, []);
+
+  // alternative: using async function
   useEffect(() => {
-    fetch("https://api.imgflip.com/get_memes")
-      .then(res => res.json())
-      .then(data => setAllMemes(data.data.memes));
+    const getMeme = async () => {
+      const res = await fetch("https://api.imgflip.com/get_memes");
+      const data = await res.json();
+      setAllMemes(data.data.memes);
+    };
+
+    getMeme();
   }, []);
 
   const getMemeImage = () => {
